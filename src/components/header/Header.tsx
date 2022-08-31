863767077a666da55e019d266e18d5b2046f3caa
hamelln <vusdus91@gmail.com>
2022-08-31 22:54:19 +0900

    fix: Link와 a 분리해서 GNB 언더바 새로고침 현상 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,24 @@
+import Link from "next/link";
 import React, { MouseEvent, useState } from "react";
 import * as S from "./Styles";
 
 const Header = () => {
   const [isActive, setIsActive] = useState(0);
+
   function onClick(e: React.MouseEvent<HTMLAnchorElement>) {
     switch (e.currentTarget.innerText) {
       case "서비스":
         setIsActive(0);
         break;
       case "뉴스룸":
+        console.log(123123);
         setIsActive(1);
         break;
       case "찾아오는 길":
         setIsActive(2);
         break;
       default:
-        setIsActive(0);
+        break;
     }
   }
 
@@ -24,27 +27,21 @@ const Header = () => {
       <S.HeaderWrpper>
         <S.BrandLogo />
         <S.MenuList>
-          <S.NavigationLink
-            href="/ko/product"
-            isActive={isActive === 0}
-            onClick={onClick}
-          >
-            서비스
-          </S.NavigationLink>
-          <S.NavigationLink
-            href="/ko/press"
-            onClick={onClick}
-            isActive={isActive === 1}
-          >
-            뉴스룸
-          </S.NavigationLink>
-          <S.NavigationLink
-            href="/ko/contact"
-            isActive={isActive === 2}
-            onClick={onClick}
-          >
-            찾아오는 길
-          </S.NavigationLink>
+          <Link href="/product">
+            <S.NavigationLink isActive={isActive === 0} onClick={onClick}>
+              서비스
+            </S.NavigationLink>
+          </Link>
+          <Link href="/press">
+            <S.NavigationLink onClick={onClick} isActive={isActive === 1}>
+              뉴스룸
+            </S.NavigationLink>
+          </Link>
+          <Link href="/contact">
+            <S.NavigationLink isActive={isActive === 2} onClick={onClick}>
+              찾아오는 길
+            </S.NavigationLink>
+          </Link>
           <S.NavigationLink
             href="https://blog.mathpresso.com/"
             target="_blank"
